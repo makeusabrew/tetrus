984b012be0979aa84e25df8c7ea1d44eee6be1f3
Nick Payne <nick@kurai.co.uk>
2022-03-19 19:00:17 +0000

    add all pieces and rotated variants

--- src/main.rs
+++ src/main.rs
@@ -17,29 +17,72 @@ const BLOCK_COUNT: usize = 200;
 const PLAYFIELD_START_X: usize = 250;
 const PLAYFIELD_START_Y: usize = 0;
 
-const T_PIECE_N: [[u8; 3]; 3] = [
-    [0, 200, 0],
-    [200, 200, 200],
-    [0, 0, 0],
+const T_PIECES: [[[u8; 3]; 3]; 4] = [
+    [[0, 1, 0], [1, 1, 1], [0, 0, 0]],
+    [[0, 1, 0], [0, 1, 1], [0, 1, 0]],
+    [[0, 0, 0], [1, 1, 1], [0, 1, 0]],
+    [[0, 1, 0], [1, 1, 0], [0, 1, 0]],
 ];
-const T_PIECE_E: [[u8; 3]; 3] = [
-    [0, 200, 0],
-    [0, 200, 200],
-    [0, 200, 0],
+const O_PIECES: [[[u8; 2]; 2]; 1] = [[[1, 1], [1, 1]]];
+const S_PIECES: [[[u8; 3]; 3]; 4] = [
+    [[0, 1, 1], [1, 1, 0], [0, 0, 0]],
+    [[0, 1, 0], [0, 1, 1], [0, 0, 1]],
+    [[0, 0, 0], [0, 1, 1], [1, 1, 0]],
+    [[1, 0, 0], [1, 1, 0], [0, 1, 0]],
 ];
-const T_PIECE_S: [[u8; 3]; 3] = [
-    [0, 0, 0],
-    [200, 200, 200],
-    [0, 200, 0],
+const Z_PIECES: [[[u8; 3]; 3]; 4] = [
+    [[1, 1, 0], [0, 1, 1], [0, 0, 0]],
+    [[0, 0, 1], [0, 1, 1], [0, 1, 0]],
+    [[0, 0, 0], [1, 1, 0], [0, 1, 1]],
+    [[0, 1, 0], [1, 1, 0], [1, 0, 0]],
 ];
-const T_PIECE_W: [[u8; 3]; 3] = [
-    [0, 200, 0],
-    [200, 200, 0],
-    [0, 200, 0],
+const L_PIECES: [[[u8; 3]; 3]; 4] = [
+    [[0, 0, 1], [1, 1, 1], [0, 0, 0]],
+    [[0, 1, 0], [0, 1, 0], [0, 1, 1]],
+    [[0, 0, 0], [1, 1, 1], [1, 0, 0]],
+    [[1, 1, 0], [0, 1, 0], [0, 1, 0]],
+];
+const J_PIECES: [[[u8; 3]; 3]; 4] = [
+    [[1, 0, 0], [1, 1, 1], [0, 0, 0]],
+    [[0, 1, 1], [0, 1, 0], [0, 1, 0]],
+    [[0, 0, 0], [1, 1, 1], [0, 0, 1]],
+    [[0, 1, 0], [0, 1, 0], [1, 1, 0]],
+];
+const I_PIECES: [[[u8; 4]; 4]; 4] = [
+    [[0, 0, 0, 0], [1, 1, 1, 1], [0, 0, 0, 0], [0, 0, 0, 0]],
+    [[0, 0, 1, 0], [0, 0, 1, 0], [0, 0, 1, 0], [0, 0, 1, 0]],
+    [[0, 0, 0, 0], [0, 0, 0, 0], [1, 1, 1, 1], [0, 0, 0, 0]],
+    [[0, 1, 0, 0], [0, 1, 0, 0], [0, 1, 0, 0], [0, 1, 0, 0]],
 ];
 
-const T_PIECES: [[[u8; 3]; 3]; 4] = [T_PIECE_N, T_PIECE_E, T_PIECE_S, T_PIECE_W];
+struct Piece {
+    pub shapes: Vec<Vec<Vec<u8>>>,
+    pub angle: usize,
+    pub column: usize,
+    pub row: usize,
+}
 
+fn map_shapes(piece_type: usize) -> Vec<Vec<Vec<u8>>> {
+    match piece_type {
+        0 => T_PIECES.map(|p| p.map(|p| p.to_vec()).to_vec()).to_vec(),
+        1 => O_PIECES.map(|p| p.map(|p| p.to_vec()).to_vec()).to_vec(),
+        2 => S_PIECES.map(|p| p.map(|p| p.to_vec()).to_vec()).to_vec(),
+        3 => Z_PIECES.map(|p| p.map(|p| p.to_vec()).to_vec()).to_vec(),
+        4 => L_PIECES.map(|p| p.map(|p| p.to_vec()).to_vec()).to_vec(),
+        5 => J_PIECES.map(|p| p.map(|p| p.to_vec()).to_vec()).to_vec(),
+        _ => I_PIECES.map(|p| p.map(|p| p.to_vec()).to_vec()).to_vec(),
+    }
+}
+
+fn get_random_piece() -> Piece {
+    let index = rand::random::<usize>() % 7;
+    Piece {
+        shapes: map_shapes(index),
+        angle: 0,
+        column: 0,
+        row: 0,
+    }
+}
 
 fn main() {
     // 10x20 area
@@ -49,7 +92,8 @@ fn main() {
 
     let video_subsystem = sdl_context.video().unwrap();
 
-    let window = video_subsystem.window("tetrus", SCREEN_WIDTH, SCREEN_HEIGHT)
+    let window = video_subsystem
+        .window("tetrus", SCREEN_WIDTH, SCREEN_HEIGHT)
         .position_centered()
         .build()
         .unwrap();
@@ -58,70 +102,79 @@ fn main() {
 
     let mut event_pump = sdl_context.event_pump().unwrap();
 
-    let mut current_piece_col: usize = 0;
-    let mut current_piece_row = 0;
-    //§let mut current_piece = [[0u8; 3]; 3];
-    let mut current_piece_index = 0;
-
     let mut tick_time = Instant::now();
+
+    let mut piece = get_random_piece();
 
     'running: loop {
         // @TODO: don't directly mutate x/y here, signal that we want to move
         for event in event_pump.poll_iter() {
             match event {
-                Event::Quit {..} |
-                Event::KeyDown { keycode: Some(Keycode::Escape), .. } => {
-                    break 'running
-                },
-                Event::KeyDown { keycode: Some(Keycode::A), .. } => {
-                    current_piece_col -= 1;
+                Event::Quit { .. }
+                | Event::KeyDown {
+                    keycode: Some(Keycode::Escape),
+                    ..
+                } => break 'running,
+                Event::KeyDown {
+                    keycode: Some(Keycode::A),
+                    ..
+                } => {
+                    piece.column -= 1;
                 }
-                Event::KeyDown { keycode: Some(Keycode::D), .. } => {
-                    current_piece_col += 1;
+                Event::KeyDown {
+                    keycode: Some(Keycode::D),
+                    ..
+                } => {
+                    piece.column += 1;
                 }
-                Event::KeyDown { keycode: Some(Keycode::S), .. } => {
-                    current_piece_row += 1;
+                Event::KeyDown {
+                    keycode: Some(Keycode::S),
+                    ..
+                } => {
+                    piece.row += 1;
                 }
-                Event::KeyDown { keycode: Some(Keycode::W), .. } => {
-                    current_piece_index += 1;
-                    if current_piece_index == 4 {
-                        current_piece_index = 0;
+                Event::KeyDown {
+                    keycode: Some(Keycode::W),
+                    ..
+                } => {
+                    piece.angle += 1;
+                    if piece.angle == piece.shapes.len() {
+                        piece.angle = 0;
                     }
                 }
                 _ => {}
             }
         }
 
-        let current_piece = T_PIECES[current_piece_index];
-
         if tick_time.elapsed().as_secs() >= 1 {
             tick_time = Instant::now();
-            current_piece_row += 1;
+            piece.row += 1;
         }
 
-        'collision: for (row_index, piece_row) in current_piece.iter().enumerate() {
-            let y = (current_piece_row + row_index) * BLOCK_PER_ROW;
+        let current_shape = piece.shapes[piece.angle as usize].clone();
+
+        'collision: for (row_index, piece_row) in current_shape.iter().enumerate() {
+            let y = (piece.row + row_index) * BLOCK_PER_ROW;
             for (col_index, piece_val) in piece_row.iter().enumerate() {
                 let colour = *piece_val;
                 if colour == 0 {
-                    continue
+                    continue;
                 }
-                let x = current_piece_col + col_index;
+                let x = piece.column + col_index;
                 let idx = (x + y) + BLOCK_PER_ROW;
-                if idx >= BLOCK_COUNT  || blocks[idx] != 0 {
+                if idx >= BLOCK_COUNT || blocks[idx] != 0 {
                     // write all the piece blocks into the background blocks array
-                    for (row_index, piece_row) in current_piece.iter().enumerate() {
-                        let row_offset = (row_index + current_piece_row) * 10;
-                        let col_offset = current_piece_col as usize;
+                    for (row_index, piece_row) in current_shape.iter().enumerate() {
+                        let row_offset = (row_index + piece.row) * 10;
+                        let col_offset = piece.column as usize;
                         for (piece_index, piece_block) in piece_row.iter().enumerate() {
                             if *piece_block == 0 {
                                 continue;
                             }
-                            blocks[row_offset + piece_index + col_offset] = *piece_block;
+                            blocks[row_offset + piece_index + col_offset] = *piece_block * 100;
                         }
                     }
-                    current_piece_row = 0;
-                    current_piece_col = 0;
+                    piece = get_random_piece();
                     break 'collision;
                 }
             }
@@ -141,22 +194,26 @@ fn main() {
             let y = PLAYFIELD_START_Y + ((i / 10) * BLOCK_SIZE);
             let rect_w = BLOCK_SIZE as u32;
             canvas.set_draw_color(Color::RGB(colour, colour, colour));
-            canvas.fill_rect(Rect::new(x as i32, y as i32, rect_w, rect_w)).unwrap();
+            canvas
+                .fill_rect(Rect::new(x as i32, y as i32, rect_w, rect_w))
+                .unwrap();
         }
 
         // current piece
-        for (row_index, piece_row) in current_piece.iter().enumerate() {
-            let y = PLAYFIELD_START_Y + ((current_piece_row + row_index) * BLOCK_SIZE);
+        for (row_index, piece_row) in current_shape.iter().enumerate() {
+            let y = PLAYFIELD_START_Y + ((piece.row + row_index) * BLOCK_SIZE);
 
             for (piece_index, piece_val) in piece_row.iter().enumerate() {
                 let colour = *piece_val;
                 if colour == 0 {
-                    continue
+                    continue;
                 }
-                let x = PLAYFIELD_START_X + ((current_piece_col + piece_index) * BLOCK_SIZE);
+                let x = PLAYFIELD_START_X + ((piece.column + piece_index) * BLOCK_SIZE);
                 let rect_w = BLOCK_SIZE as u32;
-                canvas.set_draw_color(Color::RGB(colour, colour, colour));
-                canvas.fill_rect(Rect::new(x as i32, y as i32, rect_w, rect_w)).unwrap();
+                canvas.set_draw_color(Color::RGB(colour * 100, colour, colour));
+                canvas
+                    .fill_rect(Rect::new(x as i32, y as i32, rect_w, rect_w))
+                    .unwrap();
             }
         }
         canvas.present();
